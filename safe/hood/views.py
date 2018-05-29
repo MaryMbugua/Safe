4ab9d5131c96ce985222b4fc51c341131c69b33e
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-29 13:21:46 +0300

    first commit

---
+++ safe/hood/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def landing(request):
+    return render(request,'index.html')
